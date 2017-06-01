5eb7afb5ab7336d284373fe88990d092ab46a72a
Amanda Kaufman <user.name>
2017-06-01 11:45:15 -0700

    First Commit

---
+++ MyFirstProgram.java
@@ -0,0 +1,14 @@
+package chapter1;
+/*Amanda Kaufman
+ Chapter 1
+ Programming Challenge 1
+ My First Java Program
+  Hello World!
+*/
+
+public class MyFirstProgram
+{
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
